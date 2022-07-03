964ded8d3239118a1ba4e2f551b3dcbcccd46b42
anas-dew <mailtoanas@duck.com>
2022-07-03 11:07:16 +0530

    favicon added file not supported error added no file found error added desc function removed xls not supported yet.

--- free-document-viewer/views.py
+++ free-document-viewer/views.py
@@ -1,19 +1,38 @@
-from django.http import HttpResponse
+import shutil
 from django.shortcuts import render
-from .django_app import Analysis,descriptive
+from .django_app import Analysis
+from django.utils.datastructures import MultiValueDictKeyError
+import os
+
 
 def home(request):
     if request.method == 'POST':
-        # try:
+        try:
             file = request.FILES['file']
             
-            with open(f"{file}",'wb') as f:
+            # Checking if the directory exists and if it doesn't it creates it.
+            if os.path.isdir('df') == True:
+                pass
+            else:
+                os.mkdir('df')
+
+            # Opening the file and writing the file to the directory.
+            with open(f"df/{file}", 'wb') as f:
                 f.write(file.read())
 
-            data = Analysis(f"{file}")
-            desc = descriptive(data=data)
+            data = Analysis(f"df/{file}")
 
-            return render(request, 'home/index.html',{"data":data.to_html(),"desc":desc.to_html()})
-        # except:
-        #     return HttpResponse('Error. App is in early development.')
+            return render(request, 'home/index.html', {"data": data.to_html()})
+
+        # This is a try/except block. It is trying to get the file from the request. If it fails, it
+        # will return the error message.
+        except MultiValueDictKeyError:
+            return render(request, 'home/index.html', {"data": "<h3>No file found.</h3>"})
+        except:
+            return render(request, 'home/index.html', {"data": "<h3>Some error. App is in early development...</h3>"})
+    # Trying to delete the directory. If it fails, it will pass.
+    try:
+        shutil.rmtree('./df')
+    except FileNotFoundError:
+        pass
     return render(request, 'home/index.html')
